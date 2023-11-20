05832926d7b3df83e9618959f803bedc10c55c62
sachintha180 <sachinthasenanayake180@gmail.com>
2023-11-20 12:10:05 +0530

    Update JSDoc strings and add string capitalization functions.

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 /**
  * simulates the computer player by returning a
- * random choice between "rock", "paper" and "scissors".
+ * random choice between "Rock", "Paper" and "Scissors".
  * 
- * @returns {string} - pseudorandom string; "rock", "paper" or "scissors".
+ * @returns {string} - pseudorandom string; "Rock", "Paper" or "Scissors".
  */
 
 function getComputerChoice() {
 
     // initialize array of choices
-    choices = ["rock", "paper", "scissors"];
+    let choices = ["Rock", "Paper", "Scissors"];
 
     // pick random choice
-    choice = choices[Math.floor(Math.random() * choices.length)];
+    let choice = choices[Math.floor(Math.random() * choices.length)];
 
     // return choice
     return choice;
@@ -21,8 +21,8 @@ function getComputerChoice() {
 /**
  * simulates a single round of rock paper scissors.
  * 
- * @param {string} playerSelection - the player's choice string; "rock", "paper" or "scissors".
- * @param {string} computerSelection - the computer's random choice string; "rock", "paper" or "scissors".
+ * @param {string} playerSelection - the player's choice string; rock / paper / scissors.
+ * @param {string} computerSelection - the computer's random choice string; rock / paper / scissors".
  * @returns {string} - a message string declaring the winner.
  */
 
@@ -30,23 +30,23 @@ function playRound(playerSelection, computerSelection) {
 
     // initialize object of result combinations
     const combinations = {
-        "rock": {
-            winTo: "scissors",
-            loseTo: "paper"
+        "Rock": {
+            winTo: "Scissors",
+            loseTo: "Paper"
         },
-        "paper": {
-            winTo: "rock",
-            loseTo: "scissors"
+        "Paper": {
+            winTo: "Rock",
+            loseTo: "Scissors"
         },
-        "scissors": {
-            winTo: "paper",
-            loseTo: "rock"
+        "Scissors": {
+            winTo: "Paper",
+            loseTo: "Rock"
         }
     }
 
-    // set player and computer selections to lowercase
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
+    // set player and computer selections to capitalized
+    playerSelection = capitalize(playerSelection);
+    computerSelection = capitalize(computerSelection);
 
     // determine if tie
     if (playerSelection === computerSelection) {
@@ -55,13 +55,30 @@ function playRound(playerSelection, computerSelection) {
 
     // otherwise, determine if player won
     else if (combinations[playerSelection].winTo === computerSelection) {
-        return `You Win! ${playerSelection} beats ${computerSelection}`; 
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
     }
 
     // otherwise, determine if player lost
     else if (combinations[playerSelection].loseTo === computerSelection) {
         return `You Lose! ${computerSelection} beats ${playerSelection}`;
     }
+}
+
+
+/**
+ * capitalizes the first letter of a string.
+ * 
+ * @param {string} text - mixed-case input string.
+ * @returns {string} - first letter capitalized string.
+ */
+
+function capitalize(text) {
+
+    // slice and extract portions of the string for capitalization
+    let capitalized = text.at(0).toUpperCase() + text.toLowerCase().slice(1,);
+
+    // return capitalized string
+    return capitalized;
 }
 
 
