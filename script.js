91af5f6de7127704edd535725d2bac1e79d06913
sachintha180 <sachinthasenanayake180@gmail.com>
2023-11-20 11:20:37 +0530

    Implement getComputerChoice() function in script.js

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
+/**
+ * emulates the computer player by returning a
+ * random choice between "Rock", "Paper" and "Scissors".
+ * 
+ * @return {string} a pseudorandom random string
+ */
+
+function getComputerChoice() {
+
+    // initialize array of choices
+    choices = ["Rock", "Paper", "Scissors"];
+
+    // pick random choice
+    choice = choices[Math.floor(Math.random() * choices.length)];
+
+    // return choice
+    return choice;
+}
+
 document.addEventListener("DOMContentLoaded", (event) => {
+
 
 
 })
